f96b4e330580ff10e7bc9a0cddbc3533c92b8e05
Rafael Souza <me@rafaelss.com>
2012-01-27 02:36:48 -0200

    logger and connection are "normal" methods now

--- lib/pg_queue.rb
+++ lib/pg_queue.rb
@@ -3,12 +3,25 @@ require "pg"
 require "logger"
 
 module PgQueue
-  extend self
-
   autoload :Worker, "pg_queue/worker"
   autoload :Job, "pg_queue/job"
 
-  attr_accessor :logger
-  self.logger = Logger.new(STDOUT)
-end
+  def self.logger
+    @logger ||= Logger.new(STDOUT)
+  end
 
+  def self.logger=(object)
+    @logger = object
+  end
+
+  def self.connection
+    @connection ||= begin
+      conn = PGconn.open(:dbname => 'pg_queue_test')
+    end
+  end
+
+  def self.connection=(object)
+    @connection = object
+  end
+
+end
--- spec/pg_queue_spec.rb
+++ spec/pg_queue_spec.rb
@@ -1,16 +1,35 @@
 require "spec_helper"
 
-describe PgQueue do
-  its(:logger) { should be_instance_of(Logger) }
+class MyLogger; end
+class MyDbConnection; end
 
-  context "setting a logger" do
-    after do
-      described_class.logger = Logger.new(STDOUT)
+describe PgQueue do
+  context "logging" do
+    its(:logger) { should be_instance_of(Logger) }
+
+    it "defines a new logger" do
+      described_class.logger = MyLogger.new
+      described_class.logger.should be_instance_of(MyLogger)
     end
 
-    it "should set a new logger" do
+    after do
       described_class.logger = nil
-      described_class.logger.should be_nil
+    end
+  end
+
+  context "database connection" do
+    its(:connection) { should be_instance_of(PGconn) }
+
+    it "defines a new connection" do
+      described_class.connection = MyDbConnection.new
+      described_class.connection.should be_instance_of(MyDbConnection)
+    end
+
+    after do
+      described_class.connection = nil
+    end
+  end
+
     end
   end
 end
