3d021179f93798af90e5d66392cf6e3cb8416e4d
Rafael Souza <me@rafaelss.com>
2012-01-27 22:21:33 -0200

    considering that we need to load rails environment, for now, it's better use a rake task to  start workers

--- lib/pg_queue/tasks.rb
+++ lib/pg_queue/tasks.rb
@@ -1,4 +1,17 @@
 namespace :pg_queue do
+  desc "Start worker"
+  task :work do
+    worker = PgQueue::Worker.new
+
+    ["INT", "TERM"].each do |signal|
+      trap(signal) do
+        worker.stop
+      end
+    end
+
+    worker.start
+  end
+
   desc "Drop jobs table"
   task :drop_table do
     PgQueue.connection.exec("DROP TABLE IF EXISTS pg_queue_jobs")
