14a96d0ea75e22b4c63fe53abc0872099805b3b6
Rafael Souza <me@rafaelss.com>
2012-01-27 22:20:52 -0200

    add description to drop/create tasks

--- lib/pg_queue/tasks.rb
+++ lib/pg_queue/tasks.rb
@@ -1,8 +1,10 @@
 namespace :pg_queue do
+  desc "Drop jobs table"
   task :drop_table do
     PgQueue.connection.exec("DROP TABLE IF EXISTS pg_queue_jobs")
   end
 
+  desc "Create jobs table"
   task :create_table => :drop_table do
     PgQueue.connection.exec("CREATE TABLE pg_queue_jobs (id SERIAL, class_name VARCHAR, args TEXT)")
   end
