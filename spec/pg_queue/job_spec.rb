2453f04544b4f58eafa50f11b5effe155cf4516d
Rafael Souza <me@rafaelss.com>
2012-02-03 12:13:44 -0200

    more DRY

--- spec/pg_queue/job_spec.rb
+++ spec/pg_queue/job_spec.rb
@@ -1,18 +1,22 @@
 require "spec_helper"
 
-class MyQueue; end
-
 describe PgQueue::Job do
+  let(:queue_class) { Object.const_set("OrdinaryQueue", Class.new) }
+
   subject do
     described_class.new(
       "id" => 1,
-      "class_name" => "MyQueue",
+      "class_name" => queue_class.name,
       "args" => MultiJson.encode([1, "two"])
     )
   end
 
+  after do
+    Object.send(:remove_const, queue_class.name.to_sym)
+  end
+
   its(:id) { should == 1 }
-  its(:klass) { should == MyQueue }
+  its(:klass) { should == OrdinaryQueue }
   its(:args) { should == [1, "two"] }
 
   it "performs the job" do
