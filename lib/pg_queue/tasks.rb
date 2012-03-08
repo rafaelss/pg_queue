8baff2ceeb8010165171f3be6292450971854870
Rafael Souza <me@rafaelss.com>
2012-03-08 12:24:16 -0300

    now it's possible run the worker as a daemon using pg_queue:work:start and pg_queue:work:stop

--- lib/pg_queue/tasks.rb
+++ lib/pg_queue/tasks.rb
@@ -1,6 +1,5 @@
 namespace :pg_queue do
-  desc "Start worker"
-  task :work do
+  def start_worker
     worker = PgQueue::Worker.new
 
     ["INT", "TERM"].each do |signal|
@@ -9,7 +8,14 @@ namespace :pg_queue do
       end
     end
 
+    yield
+
     worker.start
+  end
+
+  desc "Start worker"
+  task :work do |args|
+    start_worker
   end
 
   desc "Drop jobs table"
@@ -20,5 +26,26 @@ namespace :pg_queue do
   desc "Create jobs table"
   task :create_table => :drop_table do
     PgQueue.connection.exec("CREATE TABLE pg_queue_jobs (id SERIAL, class_name VARCHAR, args TEXT)")
+  end
+
+  namespace :work do
+    desc "Start worker and put it in background"
+    task :start do
+      start_worker do
+        Process.daemon(true)
+
+        pid_path = File.join(File.expand_path("."), "tmp/pids/pg_queue.pid")
+        File.open(pid_path, "w") do |f|
+          f.write(Process.pid)
+        end
+      end
+    end
+
+    desc "Stop worker"
+    task :stop do
+      pid_path = File.join(File.expand_path("."), "tmp/pids/pg_queue.pid")
+      Process.kill("TERM", File.read(pid_path).to_i)
+      File.unlink(pid_path)
+    end
   end
 end
